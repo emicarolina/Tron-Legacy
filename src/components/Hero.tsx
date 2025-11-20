91c179929b976e4d7111f59c6a0cf13be086a9e5
emicarolina <emillycarolinaceronbarbosa900@outlook.com>
2025-11-20 17:35:34 -0300

    style: updated hero titles

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,36 +1,27 @@
-import { ReactTyped } from "react-typed";
 import TronBackground from "./TronBackground";
 
 const Hero = () => {
   return (
     <section
       id="hero"
-      className="relative w-full h-screen flex items-start justify-center text-center overflow-hidden pt-20"
+      className="relative w-full h-screen flex items-start justify-center text-center overflow-hidden pt-35 "
     >
       <TronBackground />
-      <div className="text-white px-4">
+      <div className="text-white">
         <h1
           data-aos="fade-zoom-in"
           data-aos-easing="ease-in-back"
-          className="text-shadow-tron text-tron-blue text-4xl md:text-5xl font-orbitron font-bold"
+          className="text-shadow-tron text-tron-blue text-4xl md:text-7xl max-w-80 md:min-w-300 font-orbitron "
         >
-          <ReactTyped
-            strings={["Tron Legacy: o impossível se torna real."]}
-            typeSpeed={50}
-            backSpeed={30}
-            startDelay={1900}
-            backDelay={3000}
-            showCursor={true}
-            loop={true}
-          />
+          Tron Legacy: o impossível se torna real.
         </h1>
         <p
           data-aos="fade-zoom-in"
-          data-aos-delay="3000"
           data-aos-easing="ease-in-back"
-          className="text-lg pt-2 md:text-2xl font-extralight"
+          className="text-gray-300 max-w-74 md:min-w-130 mx-auto text-lg md:text-xl leading-relaxed glass-panel mt-15 p-6 rounded-sm font-st"
         >
-          Explore um universo de luz, códigos e revolução digital.
+          Entre em um mundo criado por códigos luminosos e descubra como a
+          tecnologia pode transformar realidades.
         </p>
       </div>
     </section>
