03d0e7ce1e54bdd5c276f7b5fddc461decddad18
emicarolina <emillycarolinaceronbarbosa900@outlook.com>
2025-11-20 20:47:19 -0300

    style: added new styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer className="bg-tron-blue text-white py-5 px-6 flex justify-center">
-      <h3 className="text-black font-semibold ">
+    <footer className="bg-tron-blue py-5 px-6 flex justify-center">
+      <h3 className="text-black font-semibold font-st">
         Desenvolvido por Emilly • 2025
       </h3>
     </footer>
