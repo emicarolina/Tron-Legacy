3d4c4efc84da9d37415739233cf457939cf2c00a
emicarolina <emillycarolinaceronbarbosa900@outlook.com>
2025-11-20 17:59:18 -0300

    fix: adjusted header responsive layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
 const Header = () => {
   return (
-    <header className="fixed top-0 left-0 w-full z-50 backdrop-blur-md flex items-center justify-between px-6 py-4 bg-black/30 border-b border-white/10 ">
+    <header className="fixed top-0 left-0 w-full z-50 backdrop-blur-md flex flex-col md:flex-row items-center justify-between px-6 py-4 bg-black/30 border-b border-white/10 ">
       <div className="text-tron-blue font-orbitron text-2xl font-bold">
         <a href="/">TRON: Legacy</a>
       </div>
-      <nav className="space-x-4 text-white text-lg md:flex md:space-x-8">
+      <nav className="space-x-4 text-white text-lg flex md:space-x-8">
         <a
           href="#about"
           className="font-prompt  hover:text-tron-blue transition"
